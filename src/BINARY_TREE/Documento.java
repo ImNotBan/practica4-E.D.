d56f5ef3765b8d829dbcff1bd3116b62fe37bcaa
edmon21 <edmonalkoceba21@gmail.com>
2025-11-20 17:53:00 +0100

    Tasca 1 realitzada

--- src/BINARY_TREE/Documento.java
+++ src/BINARY_TREE/Documento.java
@@ -1,4 +1,34 @@
 package BINARY_TREE;
 
-public class Documento {
+public class Documento implements Comparable<Documento> {
+
+    private int priority;   // prioritat del document
+    private String name;
+
+    // constructor
+    public Documento(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // funcions que venen de la interficie
+
+    @Override
+    public int compareTo(Documento other) {
+        return Integer.compare(this.priority, other.priority);
+    }
+
+    @Override
+    public String toString() {
+        return "Document de " + name + " amb prioritat: " + priority;
+    }
+
 }
