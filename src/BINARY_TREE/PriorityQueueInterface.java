3ec84c6220f945892aca1c5abd7577744084771c
edmon21 <edmonalkoceba21@gmail.com>
2025-11-20 17:29:27 +0100

    Prova

--- src/BINARY_TREE/PriorityQueueInterface.java
+++ src/BINARY_TREE/PriorityQueueInterface.java
@@ -2,13 +2,15 @@ package BINARY_TREE;
 
 public interface PriorityQueueInterface<E> {
 
-        boolean add(E x);
+    boolean add(E x);
 
     E remove();
 
     String toString();
 
+    // metode auxiliar per saber si el heap te elements
     boolean isEmpty();
 
+    // metode auxiliar simplement per fer testos i veure on resideixen els errors
     int size();
 }
