e8638ce4c7b3e2e06e791d4ee3f88e1f064a3d47
ImNotBan <edmondelbruc@gmail.com>
2025-11-21 15:55:59 +0100

    Implementacio remove pero queda entendre una part del codi

--- src/BINARY_TREE/MyPriorityQueue.java
+++ src/BINARY_TREE/MyPriorityQueue.java
@@ -30,10 +30,54 @@ public class MyPriorityQueue<E extends Comparable<? super E>> {
 
     }
 
+    public E remove(){
+        if(heap.isEmpty()){
+            return null;
+        }
+
+        E min = heap.get(0); // guardem el minim
+        E last = heap.remove(heapSize - 1); // eliminem ultim
+
+        if (!heap.isEmpty()) {
+            heap.set(0, last);
+            int i = 0;
+
+            boolean done = false;
+            while (!done) {
+                int left = 2 * i + 1;
+                int right = 2 * i + 2;
+                int smallest = i;
+
+                if (left < heap.size() && heap.get(left).compareTo(heap.get(smallest)) < 0) {
+                    smallest = left;
+                }
+                if (right < heap.size() && heap.get(right).compareTo(heap.get(smallest)) < 0) {
+                    smallest = right;
+                }
+
+                if (smallest != i) {
+                    E temp = heap.get(i);
+                    heap.set(i, heap.get(smallest));
+                    heap.set(smallest, temp);
+                    i = smallest;
+                } else {
+                    done = true;
+                }
+            }
+        }
+
+        return min;
+
+    }
+
     boolean isEmpty(){
         if (heapSize == 0){
             return true;
         }
         return false;
+    }
+
+    public int size () {
+        return heapSize;
     }
 }
